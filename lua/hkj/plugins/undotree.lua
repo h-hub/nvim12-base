f365df5b924d24c141c8705f7fc2825bccf2379e
h-hub <harsha.kj89@gmail.com>
2026-04-23 10:08:58 +1000

    add reset func to undotree

--- lua/hkj/plugins/undotree.lua
+++ lua/hkj/plugins/undotree.lua
@@ -1,2 +1,9 @@
 vim.pack.add({ 'https://github.com/mbbill/undotree' })
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "undotree",
+    callback = function()
+        vim.cmd("vertical resize 30")
+    end,
+})
